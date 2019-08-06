9c331995884624cd45083d6b074650df54a623bb
Jenny Oum <jennyoum@hotmail.com>
2019-08-06 14:34:10 -0400

    npm packages, files

---
+++ bamazon.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE bamazon_db;
+USE bamazon_db;
+
+CREATE TABLE products(
+    item_id 
+    product_name VARCHAR
+    department_name VARCHAR
+    price INTEGER
+    stock_quantity INTEGER
+)
+
+
