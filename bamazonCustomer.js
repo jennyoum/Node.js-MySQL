9c957bfae2304f9ba4c53705c3878d35fc1519b7
Jenny Oum <jennyoum@hotmail.com>
2019-08-08 16:38:06 -0400

    fixed inventory

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -35,10 +35,32 @@ connection.connect(function(err) {
         message: "how many units of the product they would like to buy?",
           }])
           .then(function(answer){
-              var query = "SELECT FROM products WHERE?"
-              connection.query(query, { stock_quantity: answer.stock_quantity }, function(err, res) {
+              var query = "SELECT stock_quantity FROM products WHERE?"
+              connection.query(query, { item_id : answer.product_id }, function(err, res) {
                 if (err) throw err;
-                
+                console.log(res);
+                var total = parseInt(res[0].stock_quantity) - parseInt(answer.amount);
+                console.log(total);
+                 if(res.stock_quantity < answer.amount){
+                     console.log("not enough inventory!");
+                 } else{
+                     connection.query("UPDATE products SET ? WHERE ?",
+                     [
+                       {
+                        stock_quantity : total
+                       },
+                        {
+                            item_id : answer.product_id
+                        }
+                     ]
+                     , function(err, data){
+                        if (err) throw err;
+                        var query = "SELECT price FROM products WHERE?"
+                        connection.query(query, {  }),
+                       console.log("Your amount total is ")
+                     })
+                 }
+
               })
             //   need to get product from database, 
             // check if qty is the same or greather than the user wants to buy
