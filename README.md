acaac97b339d46793d392a06d19a8bb749fcb3ec
Jenny Oum <jennyoum@hotmail.com>
2019-08-09 23:45:52 -0400

    image and readme

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Node.js-MySQL
+
+Love ordering online? Welcome to Bamazon! Browse through our inventory and make a seamless, hassle free purchase!
+
+* Enter the item ID #, and the amount you would like to purchase.
+
+* Bamazon will display the inventory level and give you a total for your purchase!
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -17,11 +17,15 @@ connection.connect(function(err) {
   function displayItems() {
       connection.query("SELECT * FROM products", function(err, res){
         if (err) throw err;
+
+        console.log( `~ Retrieved ${res.length} products, looping and displaying them... ` );
         for (var i=0; i < res.length; i++){
             console.log(res[i].item_id+"|" + res[i].product_name +"|" + res[i].price);
         }
       })
     }
+
+    // now ask user for product they want to buy and check inventory and deduct purchases.
     inquirer
       .prompt([
           {
@@ -39,26 +43,38 @@ connection.connect(function(err) {
               connection.query(query, { item_id : answer.product_id }, function(err, res) {
                 if (err) throw err;
                 console.log(res);
-                var total = parseInt(res[0].stock_quantity) - parseInt(answer.amount);
-                console.log(total);
-                 if(res.stock_quantity < answer.amount){
-                     console.log("not enough inventory!");
-                 } else{
-                     connection.query("UPDATE products SET ? WHERE ?",
-                     [
+
+                var total = Number(res[0].stock_quantity) - Number(answer.amount) ;
+                console.log( ` retreived stock_quantity (${res[0].stock_quantity}) - ${answer.amount}  = ${total}`, res);
+                if(res[0].stock_quantity >= answer.amount){
+                    connection.query("UPDATE products SET ? WHERE ?",
+                    [
+                      {
+                       stock_quantity : total
+                      },
                        {
-                        stock_quantity : total
-                       },
-                        {
-                            item_id : answer.product_id
-                        }
-                     ]
-                     , function(err, data){
-                        if (err) throw err;
-                        var query = "SELECT price FROM products WHERE?"
-                        connection.query(query, {  }),
-                       console.log("Your amount total is ")
-                     })
+                           item_id : answer.product_id
+                       }
+                    ]
+                    , function(err, data){
+                        total = parseInt(res[0].stock_quantity) - parseInt(answer.amount);
+                       if (err) throw err;
+                       
+                       var query = "SELECT price FROM products WHERE?";
+
+                       connection.query(query, {item_id : answer.product_id  }, function(err, res){
+                           if(err) throw err;
+                           console.log(res);
+
+                           // got the price for the item, now calculating the receipt.....
+                            var receipt = Number(answer.amount) * Number(res[0].price);
+                            console.log( `  Your total is $${receipt} ` );
+
+                       });
+                    })
+                    
+                 } else{
+                    console.log("not enough inventory!");
                  }
 
               })
